b92fe96aab5a30358870f10930520d531200cead
annabessant <anna@thefutureofinnovation.org>
2023-02-17 16:04:01 +0000

    add title, description and installation input of README generator

--- index.js
+++ index.js
@@ -3,19 +3,100 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
+//To store answer
+let answerObject;
 
-];
+//To create README content using https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
 
-// function to write README file
-function writeToFile(fileName, data) {
+function promptUser() {
+    return inquirer
+    .prompt([
+        
+        //Title
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project? (Required):",
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            }
+            else {
+                console.log("This is a required information. Please add your project title");
+                return false;
+            }
+        }
+    }
+
+        // Description 
+        
+    {
+        type: "input",
+        name: "description",
+        message: "What is the description of your project? (Required):",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            }
+            else {
+                console.log("This is a required information. Please add your project description");
+                return false;
+            }
+        }
+    }
+  
+           // Table of Contents STOPPED ? I     nstallation is correct below
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project? (Required):",
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            }
+            else {
+                console.log("This is a required information. Please provide installation steps");
+                return false;
+            }
+        }
+    }
+
+
+            // Installation     
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project? (Required):",
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            }
+                else {
+                    console.log("This is a required information. Please provide installation steps");
+                    return false;
+                }
+            }
+    }
+
+    ])
 }
 
-// function to initialize program
-function init() {
 
-}
 
-// function call to initialize program
-init();
+
+
+
+
+
+
+// // array of questions for user
+// const questions = [
+// ];
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
+// // function to initialize program
+// function init() {
+// }
+// // function call to initialize program
+// init();
